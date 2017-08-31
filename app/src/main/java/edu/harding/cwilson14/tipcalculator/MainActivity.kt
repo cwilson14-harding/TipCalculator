f2069c73cbb15e165b2b35382fc9a1cc1298db1e
CJ Wilson <cwilson14@harding.edu>
2017-08-31 15:35:58 -0500

    Added project

---
+++ app/src/main/java/edu/harding/cwilson14/tipcalculator/MainActivity.kt
@@ -0,0 +1,54 @@
+// Name: Curtis Wilson
+// Project: Tip Calculator
+// COMP 475 (Android)
+package edu.harding.cwilson14.tipcalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import java.text.NumberFormat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Create the text watcher anonymous class using an object expression.
+        val textWatcher = object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun afterTextChanged(p0: Editable?) {
+                calculate()
+            }
+        }
+
+        // Set the new text watcher as a listener.
+        inputBill.addTextChangedListener(textWatcher)
+        inputTipPercent.addTextChangedListener(textWatcher)
+    }
+
+    // Update the calculations.
+    fun calculate() {
+        try {
+            // Calculate the answers.
+            val bill = inputBill.text.toString().toFloat()
+            val tipAmount = bill * inputTipPercent.text.toString().toFloat()/100
+            val numberFormat = NumberFormat.getCurrencyInstance()
+            val total = bill + tipAmount
+
+            // Update the labels with the calculations.
+            labelTipAmount.text = numberFormat.format(tipAmount)
+            labelTotal.text = numberFormat.format(total)
+
+        } catch (e: NumberFormatException) {
+            // Numbers failed to parse.
+            Toast.makeText(this, "Enter numbers for bill and tip percent.", Toast.LENGTH_SHORT).show()
+            labelTipAmount.text = ""
+            labelTotal.text = ""
+        }
+    }
+}
